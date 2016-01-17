ced5d4dc33de86a5acdf5372ddcac0e9844b121c
Arko <dotuplink@gmail.com>
2016-01-16 20:57:50 -0800

    Made a function of out breaking repeating key xor
    Made a function of out breaking repeating key xor

--- crypto.rs
+++ crypto.rs
@@ -423,3 +423,67 @@ pub fn hamming_distance(a: Vec<u8>, b: Vec<u8>) -> Result<i32, String>
 	Ok(hamming_distance)
 }
 
+#[allow(dead_code)]
+pub fn break_repeating_key_xor(byte_array: Vec<u8>, min: usize, max: usize) -> Result<Vec<u8>, String>
+{
+
+    // Find the key length
+    let mut best_keylength: usize = 0;
+    let mut best_avg: f32 = 8.0;
+
+    for keysize in min..max
+    {
+    	let mut avg: f32 = 0.0;
+
+    	for i in 0..byte_array.len()/keysize -1
+    	{
+    		let mut a: Vec<u8> = vec![];
+    		let mut b: Vec<u8> = vec![];
+    		
+    		for x in 0..keysize
+    		{
+    			a.push(byte_array[i*keysize+x]);
+    		}
+
+    		for y in keysize..keysize*2
+    		{
+    			b.push(byte_array[i*keysize+y]);
+    		}
+    		
+    		let mut out: f32 = hamming_distance(a.clone(),b.clone()).unwrap() as f32;
+    		out = out / (((keysize as i32)) as f32);
+
+    		avg = avg + out;
+    	}
+
+    	avg = avg / (byte_array.len() as f32/keysize as f32);
+
+    	if avg < best_avg
+    	{
+    		best_keylength = keysize;
+    		best_avg = avg;
+    	}
+    }
+
+	let mut decoded_key: Vec<u8> = vec![];
+
+	// Crack single-byte XOR key
+	for x in 0..best_keylength
+	{
+		// Generate byte array of each repeating nth byte
+		let mut a: Vec<u8> = vec![];
+		
+		for i in 0..byte_array.len()/best_keylength
+		{
+			a.push(byte_array[i*best_keylength + x]);
+		}
+
+		let (single_byte, _score): (u8,f32) = find_single_byte_xor(a.clone()).unwrap();
+		
+		decoded_key.push(single_byte);
+	}
+	
+	Ok(decoded_key)
+}
+
+
--- ex6.rs
+++ ex6.rs
@@ -24,66 +24,8 @@ fn main()
 		}
     }
 
-    // Find the key length
-    let mut best_keylength: usize = 0;
-    let mut best_avg: f32 = 8.0;
-
-    for keysize in 2..40
-    {
-    	let mut avg: f32 = 0.0;
-
-    	for i in 0..byte_array.len()/keysize -1
-    	{
-    		let mut a: Vec<u8> = vec![];
-    		let mut b: Vec<u8> = vec![];
-    		
-    		for x in 0..keysize
-    		{
-    			a.push(byte_array[i*keysize+x]);
-    		}
-
-    		for y in keysize..keysize*2
-    		{
-    			b.push(byte_array[i*keysize+y]);
-    		}
-    		
-    		let mut out: f32 = crypto::hamming_distance(a.clone(),b.clone()).unwrap() as f32;
-    		out = out / (((keysize as i32)) as f32);
-
-    		avg = avg + out;
-    	}
-
-    	avg = avg / (byte_array.len() as f32/keysize as f32);
-
-    	if avg < best_avg
-    	{
-    		best_keylength = keysize;
-    		best_avg = avg;
-    	}
-    }
-
-	//println!("{}", best_keylength);
-
-	let mut decoded_key: Vec<u8> = vec![];
-
-	// Crack single-byte XOR key
-	for x in 0..best_keylength
-	{
-		// Generate byte array of each repeating nth byte
-		let mut a: Vec<u8> = vec![];
-		
-		for i in 0..byte_array.len()/best_keylength
-		{
-			a.push(byte_array[i*best_keylength + x]);
-		}
-
-		let (single_byte, score): (u8,f32) = crypto::find_single_byte_xor(a.clone()).unwrap();
-		
-		decoded_key.push(single_byte);
-	}
+    let decoded_key = crypto::break_repeating_key_xor(byte_array.clone(), 2, 40).unwrap();
 
 	let decoded_key_string = unsafe {str::from_utf8_unchecked(&decoded_key)};
 	println!("{}", decoded_key_string);
 }
-
-
